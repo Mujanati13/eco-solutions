e60c2dd895d9791e7b4f7bf9c6a261c09605dc22
mujanati13 <simojanati.1j@gmail.com>
2025-07-10 13:00:12 +0100

    c

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    allowedHosts: ['api.albech.me'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
